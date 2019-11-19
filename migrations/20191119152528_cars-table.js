86b00a3c626641ebf7793213bd27377ebddb789a
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-11-19 15:56:56 -0600

    Schema and config

---
+++ migrations/20191119152528_cars-table.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+    tble
+      .text("make")
+      .unique()
+      .notNullable();
+    tbl
+      .text("model")
+      .unique()
+      .notNullable();
+    tbl.integer("year").notNullable();
+  });
+  //change we want to make
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+  //undoing change
+};

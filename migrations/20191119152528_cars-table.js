3ed14cd1f56890991066b548bf621c6a8f3855b4
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-11-19 16:15:45 -0600

    MVP

--- migrations/20191119152528_cars-table.js
+++ migrations/20191119152528_cars-table.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("cars", tbl => {
     tbl.increments();
-    tble
+    tbl
       .text("make")
       .unique()
       .notNullable();
